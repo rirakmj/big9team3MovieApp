b8e50eff586c1b5626845bb3329ea1eaedcb9fbf
KIM MINJI <riramj801@gmail.com>
2023-01-31 16:36:41 +0900

    민지

--- app/src/main/java/com/cookandroid/big9team3movieapp/MainActivity.java
+++ app/src/main/java/com/cookandroid/big9team3movieapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.cookandroid.big9team3movieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private String key = "65f7606d63ef23b56c0e34f0eb4d1270";
+    private String url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json";
+    ArrayList<String> title = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView listViewMain = findViewById(R.id.listViewMain);
+
     }
+
+
 }
